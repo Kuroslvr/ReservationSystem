a44bf7797e648a272596fff6ab558ba775cdfbe2
Kurosluvr <jandante1234@gmail.com>
2023-11-12 18:31:25 -0500

    Cambios

--- src/test/java/service/ReservationSystemTest.java
+++ src/test/java/service/ReservationSystemTest.java
@@ -3,7 +3,8 @@ package service;
 import model.ConferenceRoom;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ReservationSystemTest {
